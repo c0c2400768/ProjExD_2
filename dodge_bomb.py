3eb85adcce688ebdf52f226699130b9548e57b62
Haruya Iwama <c0c2400768@edu.teu.ac.jp>
2025-11-25 14:06:24 +0900

    練習問題:2

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,19 +1,30 @@
 import os
 import sys
 import pygame as pg
+import random
 
 
 WIDTH, HEIGHT = 1100, 650
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
-
+DELTA = {
+    pg.K_UP: (0, -5), 
+    pg.K_DOWN: (0, 5), 
+    pg.K_LEFT: (-5, 0), 
+    pg.K_RIGHT: (5, 0),
+}
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("fig/pg_bg.jpg")    
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
+    bb_img = pg.Surface((20, 20))
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
+    bb_img.set_colorkey((0, 0, 0))
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+    bb_rct = bb_img.get_rect()
+    bb_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -24,7 +35,7 @@ def main():
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
-        DELTA = {pg.K_UP: (0, -5), pg.K_DOWN: (0, 5), pg.K_LEFT: (-5, 0), pg.K_RIGHT: (5, 0)}
+
         for k, mv in DELTA.items():
             if key_lst[k]:
                 sum_mv[0] += mv[0]
@@ -32,6 +43,8 @@ def main():
 
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
+        bb_rct.move_ip(5, 5)
+        screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
